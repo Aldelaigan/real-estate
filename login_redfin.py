ee0e43f4ed8ec01e43449913aa94ae42e9a70e3b
alecmyres <alec.myres@gmail.com>
2017-08-10 06:58:00 -0400

    Fix login submission

--- login_redfin.py
+++ login_redfin.py
@@ -1,4 +1,6 @@
 
+# Log into Redfin
+
 # Alec Myres
 # August 2017
 
@@ -13,14 +15,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-# Selenium/PhantomJS test
-#driver = webdriver.PhantomJS(executable_path =
-#         '/usr/local/bin/phantomjs-2.1.1-macosx/bin/phantomjs')
-#driver.get('http://pythonscraping.com/pages/javascript/ajaxDemo.html')
-#time.sleep(3)
-#print driver.find_element_by_id('content').text
-#driver.close()
-
 #  Load main page
 url = 'http://www.redfin.com'
 chrome_path = '/Users/alecmyres/Applications/chromedriver'
@@ -30,10 +24,8 @@ driver = webdriver.Chrome(executable_path = chrome_path,
                           chrome_options  = chrome_options)
 driver.get(url)
 time.sleep(1)
-#page_source = driver.page_source
-#soup = BeautifulSoup(page_source, 'lxml')
 
-#  Click "Sign In"
+# Click "Sign In"
 try:
     element = WebDriverWait(driver, 10).until(
         ec.presence_of_element_located((By.LINK_TEXT, 'Sign In')))
@@ -41,7 +33,7 @@ finally:
     signin_link = driver.find_element_by_link_text('Sign In')
     signin_link.click()
 
-#  Click "Continue with email"
+# Click "Continue with email"
 try:
     element = WebDriverWait(driver, 10).until(
         ec.presence_of_element_located((By.CLASS_NAME, 'emailSignInButton')))
@@ -51,19 +43,18 @@ finally:
 
 # Enter email, password, sign in
 try:
+    driver.maximize_window()
     element = WebDriverWait(driver, 10).until(
         ec.presence_of_element_located((By.CLASS_NAME, 'input')))
 finally:
-    email = ''
-    passw = ''
-
-    # ISSUES HERE...
-    # "ElementNotVisibleException"
-    input1 = driver.find_element_by_name('emailInput')
+    email = '' # sys.argv[1]
+    passw = '' # sys.argv[2]
+    input1 = driver.find_element_by_xpath("//input[@placeholder='Email']")
+    input2 = driver.find_element_by_xpath("//input[@placeholder='Password']")
     input1.send_keys(email)
-
-    submit_button = driver.find_element_by_class_name('submitButton')
-    submit_button.click()
+    input2.send_keys(passw)
+    submit = driver.find_element_by_class_name('submitButton')
+    submit.click()
 
 # Perform search
 
