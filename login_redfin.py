87a8466e65f60adfa280d99024371cb1163f5366
alecmyres <alec.myres@gmail.com>
2017-08-10 21:43:51 -0400

    more refactoring

--- login_redfin.py
+++ login_redfin.py
@@ -15,49 +15,66 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-#  Load main page
+
+def load_main_page(url, crome_path):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path = chrome_path,
+                              chrome_options  = chrome_options)
+    driver.get(url)
+    try:
+        element = WebDriverWait(driver, 15).until(
+            ec.presence_of_element_located((By.CLASS_NAME, 'OneLineHeader')))
+    finally:
+        return driver
+
+def sign_in_redfin(driver, email, password):
+    try:
+        element = WebDriverWait(driver, 15).until(
+            ec.presence_of_element_located((By.LINK_TEXT, 'Sign In')))
+    finally:
+        signin_link = driver.find_element_by_link_text('Sign In')
+        signin_link.click()
+    try:
+        element = WebDriverWait(driver, 15).until(
+            ec.presence_of_element_located((By.CLASS_NAME, 'emailSignInButton')))
+    finally:
+        signin_email = driver.find_element_by_class_name('emailSignInButton')
+        signin_email.click()
+    try:
+        driver.maximize_window()
+        element = WebDriverWait(driver, 15).until(
+            ec.presence_of_element_located((By.CLASS_NAME, 'input')))
+    finally:
+        input1 = driver.find_element_by_xpath("//input[@placeholder='Email']")
+        input2 = driver.find_element_by_xpath("//input[@placeholder='Password']")
+        input1.send_keys(email)
+        input2.send_keys(password)
+        submit = driver.find_element_by_class_name('submitButton')
+        submit.click()
+        return driver
+
+def get_sale_pages_count(driver, zipcode, months):
+    url = 'https://www.redfin.com'
+    m = str(months)
+    z = str(zipcode)
+    search_url = url + '/zipcode/' + z + '/filter/include=sold-' + m + 'mo'
+    driver.get(search_url)
+    p = driver.find_element_by_class_name('pageText').text
+    return p.split(' ')[4]
+
+# --------------------
+# MAIN
+# --------------------
+
 url = 'http://www.redfin.com'
 chrome_path = '/Users/alecmyres/Applications/chromedriver'
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(executable_path = chrome_path,
-                          chrome_options  = chrome_options)
-driver.get(url)
-time.sleep(1)
+email = '' # sys.argv[1]
+password = '' # sys.argv[2]
+driver = load_main_page(url, chrome_path)
+driver = sign_in_redfin(driver, email, password)
 
-# Click "Sign In"
-try:
-    element = WebDriverWait(driver, 10).until(
-        ec.presence_of_element_located((By.LINK_TEXT, 'Sign In')))
-finally:
-    signin_link = driver.find_element_by_link_text('Sign In')
-    signin_link.click()
-
-# Click "Continue with email"
-try:
-    element = WebDriverWait(driver, 10).until(
-        ec.presence_of_element_located((By.CLASS_NAME, 'emailSignInButton')))
-finally:
-    signin_email = driver.find_element_by_class_name('emailSignInButton')
-    signin_email.click()
-
-# Enter email, password, sign in
-try:
-    driver.maximize_window()
-    element = WebDriverWait(driver, 10).until(
-        ec.presence_of_element_located((By.CLASS_NAME, 'input')))
-finally:
-    email = '' # sys.argv[1]
-    passw = '' # sys.argv[2]
-    input1 = driver.find_element_by_xpath("//input[@placeholder='Email']")
-    input2 = driver.find_element_by_xpath("//input[@placeholder='Password']")
-    input1.send_keys(email)
-    input2.send_keys(passw)
-    submit = driver.find_element_by_class_name('submitButton')
-    submit.click()
-
-# Perform search
-
-# Get results
+# Search (zipcode, months)
+search_url = 'https://www.redfin.com/zipcode/84095/filter/include=sold-6mo'
 
 driver.close()
