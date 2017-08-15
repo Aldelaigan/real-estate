2ea659f5efb125767d867b82fd7ebc72ae3210d5
alecmyres <alec.myres@gmail.com>
2017-08-14 22:48:40 -0400

    past sales search redfin

--- login_redfin.py
+++ login_redfin.py
@@ -54,27 +54,75 @@ def sign_in_redfin(driver, email, password):
         submit.click()
         return driver
 
-def get_sale_pages_count(driver, zipcode, months):
+def get_sale_search_url(zipcode, months):
+    # months = {3,6,12,24}
     url = 'https://www.redfin.com'
-    m = str(months)
     z = str(zipcode)
-    search_url = url + '/zipcode/' + z + '/filter/include=sold-' + m + 'mo'
-    driver.get(search_url)
-    p = driver.find_element_by_class_name('pageText').text
-    return p.split(' ')[4]
+    if months in [12,24]:
+        y = int(months) / 12
+        y = str(y)
+        search_url = url + '/zipcode/' + z + '/filter/include=sold-' + y + 'yr'
+        return search_url
+    elif months in [3,6]:
+        m = str(months)
+        search_url = url + '/zipcode/' + z + '/filter/include=sold-' + m + 'mo'
+        return search_url
+    else:
+        return 'Error with months'
+
+def get_sale_pages_count(driver, search_url):
+    try:
+        driver.get(search_url)
+        p = driver.find_element_by_class_name('pageText').text
+        return int(p.split(' ')[4])
+    except:
+        return 'Error finding pages count'
+
+def get_sale_pages_urls(search_url, p_last):
+    urls = [search_url]
+    for page in range(p_last + 1)[2:]:
+        u1 = search_url + '/page-' + str(page)
+        urls.append(u1)
+    return urls
+
+def get_sale_ids(driver, urls):
+    id_list = []
+    for url in urls:
+        print url
+        try:
+            driver.get(url)
+            element = WebDriverWait(driver, 15).until(
+                ec.presence_of_element_located((By.CLASS_NAME, 'link')))
+        finally:
+            time.sleep(np.random.uniform(0.5, 1.0))
+            driver.maximize_window()
+            links = driver.find_elements_by_xpath("//a[@class='link']") 
+            for link in links:
+                sale_path = link.get_property('href')
+                if sale_path not in id_list:
+                    id_list.append(sale_path)
+    return id_list
 
 # --------------------
 # MAIN
 # --------------------
 
 url = 'http://www.redfin.com'
+zipcode = 84095
+months = 12
 chrome_path = '/Users/alecmyres/Applications/chromedriver'
-email = '' # sys.argv[1]
-password = '' # sys.argv[2]
+creds = pd.read_csv('credentials.csv', index_col = 'SITE')
+email = creds['USERNAME']['Redfin']
+pword = creds['PASSWORD']['Redfin']
 driver = load_main_page(url, chrome_path)
-driver = sign_in_redfin(driver, email, password)
+driver = sign_in_redfin(driver, email, pword)
+s_url  = get_sale_search_url(zipcode, months)
+p_last = get_sale_pages_count(driver, search_url)
+urls   = get_sale_pages_urls(search_url, p_last)
+id_list = get_sale_ids(driver, urls) 
 
-# Search (zipcode, months)
-search_url = 'https://www.redfin.com/zipcode/84095/filter/include=sold-6mo'
+
+driver.get(search_url)
+
 
 driver.close()
